9825aa22a0622e789c4cd67e8caa771d091fa11c
cardaguh <cardaguh@gmail.com>
2020-10-11 17:57:10 -0500

    Added ui and packages

---
+++ app/src/main/java/co/cyclopsapps/mvvmpractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.cyclopsapps.mvvmpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.cyclopsapps.mvvmpractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/co/cyclopsapps/mvvmpractice/presentation/MainFragment.kt
@@ -0,0 +1,37 @@
+package co.cyclopsapps.mvvmpractice.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.cyclopsapps.mvvmpractice.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupRecyclerView() {
+        TODO("Not yet implemented")
+    }
+}
